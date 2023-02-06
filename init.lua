47eeec0fc918a8da37ea982dd7179ede0f7bcb3a
Nikita Tsybliyenko <nikita.dzer.g@gmail.com>
2023-02-06 23:07:36 +0300

    [add]: Last place cursor support.

--- init.lua
+++ init.lua
@@ -1,3 +1,4 @@
-require("color")
-require("remap")
-require("set")
+require("collector") -- collect vim-scripts
+require("color")     -- configure color scheme
+require("remap")     -- configure keymaps
+require("set")       -- configure Neovim options
---
+++ lua/collector.lua
@@ -0,0 +1,2 @@
+-- Remember last cursor position.
+vim.cmd[[ source ~/.config/nvim/vim-scripts/vim-lastplace.vim ]]
